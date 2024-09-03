a26c3508b92644b5d8871b27271782a7acbde8bc
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-09-02 18:05:55 -0600

    feat/I added crud VolunteerProfile, also  I eliminated some endpoints

---
+++ Entities/Informative/VolunteerProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Informative
+{
+    public class VolunteerProfile
+    {
+        public int Id_Volunteer_Profile { get; set; }
+        public string Tittle_Volunteer_Profile { get; set; }
+        public string Description_Volunteer_Profile { get; set; }
+    }
+}
---
+++ Nuestra Senora del Rosario/Controllers/Informative/VolunteerProfileController.cs
@@ -0,0 +1,70 @@
+﻿using Entities.Informative;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Services.Informative.GenericRepository;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VolunteerProfileController : ControllerBase
+{
+    private readonly ISvGenericRepository<VolunteerProfile> _volunteerProfileService;
+
+    public VolunteerProfileController(ISvGenericRepository<VolunteerProfile> volunteerProfileService)
+    {
+        _volunteerProfileService = volunteerProfileService;
+    }
+
+    // GET: api/VolunteerProfile
+    [HttpGet]
+    public async Task<IActionResult> GetVolunteerProfiles()
+    {
+        var items = await _volunteerProfileService.GetAllAsync();
+        return Ok(items);
+    }
+
+    // GET: api/VolunteerProfile/{id}
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetVolunteerProfile(int id)
+    {
+        var item = await _volunteerProfileService.GetByIdAsync(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        return Ok(item);
+    }
+
+    // POST: api/VolunteerProfile
+    [HttpPost]
+    public async Task<IActionResult> AddVolunteerProfile(VolunteerProfile volunteerProfile)
+    {
+        await _volunteerProfileService.AddAsync(volunteerProfile);
+        await _volunteerProfileService.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetVolunteerProfile), new { id = volunteerProfile.Id_Volunteer_Profile }, volunteerProfile);
+    }
+
+    // PATCH: api/VolunteerProfile/{id}
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchVolunteerProfile(int id, [FromBody] JsonPatchDocument<VolunteerProfile> patchDoc)
+    {
+        if (patchDoc == null)
+        {
+            return BadRequest();
+        }
+
+        await _volunteerProfileService.PatchAsync(id, patchDoc);
+        await _volunteerProfileService.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    // DELETE: api/VolunteerProfile/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVolunteerProfile(int id)
+    {
+        await _volunteerProfileService.DeleteAsync(id);
+        await _volunteerProfileService.SaveChangesAsync();
+        return NoContent();
+    }
+}
