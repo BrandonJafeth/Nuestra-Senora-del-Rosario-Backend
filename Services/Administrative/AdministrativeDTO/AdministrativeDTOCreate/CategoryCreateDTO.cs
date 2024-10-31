8745790fa267181bcf242bce88c0fb816ea00d67
RoberthCascante <robert.cascante.lopez@est.una.ac.cr>
2024-10-31 00:36:07 -0600

    feat/ categorias

---
+++ Services/Administrative/AdministrativeDTO/AdministrativeDTOCreate/CategoryCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Administrative.AdministrativeDTO.AdministrativeDTOCreate
+{
+   public class CategoryCreateDTO
+    {
+        public string CategoryName { get; set; }
+    }
+}
